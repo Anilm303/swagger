e6fe5066f2a74183838f17c51ff94057bdd5ada5
Anilm303 <anilmr1515@gmail.com>
2026-04-16 13:54:51 +0545

    add vercel api handler

--- api/index.py
+++ api/index.py
@@ -1,6 +1,16 @@
 import os
+import django
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+django.setup()
+
+# Run migrations automatically
+try:
+    call_command('migrate', verbosity=0)
+except Exception as e:
+    print("Migration error:", e)
 
 app = get_wsgi_application()
